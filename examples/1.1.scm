a15f913cf0789c6633fd7ebf6d98237976a5dcfc
Anton Kovbasa <antonkovbasa@gmail.com>
2019-11-03 21:29:58 +0200

    Update old exercises

--- examples/1.1.scm
+++ examples/1.1.scm
@@ -48,4 +48,30 @@ cincumference
 (>and= 10 4)
 
 
+#newton-method
+(define (square x)
+  (* x x))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good_enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+
+(define (sqrt-iter guess x)
+  (if (good_enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x)))
+
+
+(define (sqrt1 x)
+  (sqrt-iter 1.0 x))
+
+(sqrt1 9)
+
+
 
